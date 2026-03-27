44ca89e2b0c1729690066dbd9901c9c68153cb1b
onyz <zeid.asseh@gmail.com>
2026-03-27 22:59:50 +0300

    doc(value): error documentation improvement

--- internal/value/errors.go
+++ internal/value/errors.go
@@ -5,6 +5,6 @@ import "errors"
 var (
 	// ErrUnsupportedType is returned when an unsupported type is encountered.
 	ErrUnsupportedType = errors.New("unsupported type")
-	// ErrMustHaveElemType is returned when a list type does not have an element type defined.
-	ErrMustHaveElemType = errors.New("list type must have an element type")
+	// ErrMustHaveElemType is returned when a type that requires an element type (like a list) is missing the Elem field.
+	ErrMustHaveElemType = errors.New("must have an element type")
 )
